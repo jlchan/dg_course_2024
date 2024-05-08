2014790cf56272cc5040bf55fc528e486f394d39
Jesse Chan <jesse.lee.chan@gmail.com>
2024-05-08 12:02:42 -0500

    add modified 2D advection equation

--- advec_2d.jl
+++ advec_2d.jl
@@ -3,7 +3,8 @@ using OrdinaryDiffEq
 using Plots
 
 rd = RefElemData(Tri(), SBP(), N=3)
-md = MeshData(uniform_mesh(Tri(), 32), rd)
+md = MeshData(uniform_mesh(Tri(), 32), rd; 
+              is_periodic=true)
 
 (; x, y) = md
 u = @. sin(pi * x) * sin(pi * y)
@@ -13,24 +14,25 @@ function rhs!(du, u, p, t)
 
     uM = rd.Vf * u
     uP = uM[md.mapP]
-    u_fluxJ = @. 0.5 * (uP + uM) * md.nxJ
+    u_fluxJ = @. 0.5 * (uP + uM) * (md.nxJ + 0.5 * md.nyJ)
 
     # dudx = dr/dx * du/dr + ds/dx * du/ds 
     dudr = rd.Dr * u
     duds = rd.Ds * u
     dudxJ = @. md.rxJ * dudr + md.sxJ * duds
+    dudyJ = @. md.ryJ * dudr + md.syJ * duds
 
-    du .= dudxJ + rd.LIFT * (u_fluxJ - uM .* md.nxJ)
+    du .= dudxJ + 0.5 * dudyJ + rd.LIFT * (u_fluxJ - uM .* (md.nxJ + 0.5 * md.nyJ))
     du ./= -md.J
 end
 
 tspan = (0.0, 2.0)
 ode = ODEProblem(rhs!, u, tspan, (; rd, md))
-sol = solve(ode, Tsit5(), abstol=1e-12, reltol=1e-10,
+sol = solve(ode, Tsit5(), abstol=1e-10, reltol=1e-8,
             saveat = LinRange(tspan..., 50))
 
 @gif for u in sol.u
-    scatter(md.xyz..., zcolor=u, leg=false, ylims=(-.5, 1.5))
+    scatter(md.xyz..., zcolor=u, leg=false, ratio=1, msw=0, ms=2)
 end
 
 
